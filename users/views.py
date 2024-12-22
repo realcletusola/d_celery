5ac4cc2a7ec4cb38b4246053af58802cf02768e4
cletus <cletushunsu@gmail.com>
2024-12-22 14:57:29 +0100

    created register and login view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from .tasks import send_email_task
 
-# Create your views here.
+def user_registration(request):
+    # simulate user registration
+    user = User.objects.create_user(username='testuser', email='testemail@email.com', password='testUSER123$$')
+
+    # call celery task
+    send_email_task.delay(
+        subject="Welcome to D_celery Platform",
+        message= "Thank you for joining us",
+        recipient_list=[user.email],
+    )
+
+    return JsonResponse({"message": "User registered successfully"})
+
+def user_login(request):
+    # simulate login logic
+    user = User.objects.get(username='testuser')
+
+    # call celery task
+    send_email_task.delay(
+        subject="Login Notification",
+        message="You just logged into your account!",
+        recipient_list=[user.email],
+    )
+
+    return JsonResponse({"message":"Login successful"})
