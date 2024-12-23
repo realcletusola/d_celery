d457d313dec3acce062748801dc803c84a2cb7f0
cletus <cletushunsu@gmail.com>
2024-12-23 10:35:09 +0100

    configured email backend

--- d_celery/settings.py
+++ d_celery/settings.py
@@ -112,3 +112,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'webmaster@example.com'
+
